62f09e3a7c869359fe4ccb072963b737fc342bdd
Francesco-Matteucci <francescomatteucci31@gmail.com>
2024-11-11 12:52:46 +0100

    destroying Minchiellos work

--- database/seeders/PokemonSeeder.php
+++ database/seeders/PokemonSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Pokemon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,8 @@ class PokemonSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Pokemon::create(['name' => 'Pikachu', 'type' => 'Elettro']);
+        Pokemon::create(['name' => 'Charmander', 'type' => 'Fuoco']);
+        Pokemon::create(['name' => 'Bulbasaur', 'type' => 'Erba']);
     }
 }
--- resources/views/a-team/index.blade.php
+++ resources/views/a-team/index.blade.php
@@ -1,9 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
-        <h1>
-            Questa è la index
-        </h1>
-    </div>
+<div class="container mt-5">
+    <h1 class="text-center mb-4">Lista dei Pokémon da distruggere a minichiello</h1>
+
+    @if ($pokemons->isEmpty())
+        <p class="text-center">Nessun Minichiello è stato maltrattato.</p>
+    @else
+        <table class="table table-striped table-bordered">
+            <thead class="table-dark">
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Tipo</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($pokemons as $pokemon)
+                    <tr>
+                        <td>{{ $pokemon->id }}</td>
+                        <td>{{ $pokemon->name }}</td>
+                        <td>{{ $pokemon->type }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+</div>
+@endsection
+
 @endsection
