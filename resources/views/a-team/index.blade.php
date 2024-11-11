9cb413c071ea2f45af2ca1e43791bf672475731e
matteo-cota <matteocota@outlook.it>
2024-11-11 12:52:03 +0100

    index fix

--- resources/views/a-team/index.blade.php
+++ resources/views/a-team/index.blade.php
@@ -3,13 +3,13 @@
 @section('content')
     <div>
         <h1>
-            Questa è la index
+            Questa è la index a-team
         </h1>
     </div>
     <div class="container">
         <div class="col">
             <div class="row fw-bold">
-                Ciao come stai?
+                Ciaooooo
             </div>
         </div>
 
