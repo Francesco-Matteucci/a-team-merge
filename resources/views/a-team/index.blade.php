8f72e63289a6a9182a9bae9e2c5858ce679d0946
AlessioMartella7 <alessio.mart87@gmail.com>
2024-11-11 12:50:40 +0100

    added True index

--- resources/views/a-team/index.blade.php
+++ resources/views/a-team/index.blade.php
@@ -6,4 +6,10 @@
             Questa è la index
         </h1>
     </div>
+
+    <div class="row">
+        <div class="col-12 text-black">
+            <h1>Sicuramente questa è la vera index!!</h1>
+        </div>
+    </div>
 @endsection
