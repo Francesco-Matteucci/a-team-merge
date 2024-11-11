cbf97fac9e9a43e9fb4a8f09655c40311531f68f
FrancescoMinichiello <fraps2278@gmail.com>
2024-11-11 12:49:23 +0100

    Added div

--- resources/views/a-team/index.blade.php
+++ resources/views/a-team/index.blade.php
@@ -6,4 +6,12 @@
             Questa è la index
         </h1>
     </div>
+    <div class="container">
+        <div class="col">
+            <div class="row fw-bold">
+                Ciao come stai?
+            </div>
+        </div>
+
+    </div>
 @endsection
